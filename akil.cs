8c1f9e965c9c306bcdea98f38452f9c7609d2bb9
Akil <141535206+Akil-1@users.noreply.github.com>
2024-09-08 00:16:09 -0300

    Add files via upload

---
+++ akil.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class akil : MonoBehaviour
+{
+  public int saudedogogado = 10;
+  public float focaDoPulo = 1.00000F;
+  public double myDuble = 1.2222;
+  public string nimeDojogado = "Akil";
+  public char myChar = 'A';
+  public bool vivo = true;
+
+  GameObject objeto;
+  Color cor;
+  Light luz;
+  Camera cam;
+  Vector2 doisEixos;
+  Vector3 tresEixos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      // Debug.Log("Sua mensagem aqui");
+    }
+}
